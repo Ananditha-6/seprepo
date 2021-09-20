7000169f7ef5031f36bd1e2189e8a4a906704332
Ananditha-6 <nomulaananditha1410@gmail.com>
2021-09-20 15:12:26 +0530

    test class added

---
+++ demo/src/demo/Testclass.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Testclass {
+public void test() {
+	System.out.println("Test class validated");
+	System.out.println("All test functionalities gors here......");
+System.out.println("All test functionalities gors here......");
+
+}}
