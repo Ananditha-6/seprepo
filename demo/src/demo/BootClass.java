901fded96126c824d4b143e17258f870a2814a86
Ananditha-6 <nomulaananditha1410@gmail.com>
2021-09-20 11:01:05 +0530

    show method is added

--- demo/src/demo/BootClass.java
+++ demo/src/demo/BootClass.java
@@ -3,8 +3,12 @@ package demo;
 public class BootClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 System.out.println("Hello from STS");
-	}
+show();}
+public static void show() {
+	System.out.println("show method here");
+	
+}
+	
 
 }
